a800f3a44a0aceeed7476231de4bcbfd3680ea99
Vishnu Palanisamy <88981273+palavish@users.noreply.github.com>
2022-12-14 20:21:34 +0530

    Add files via upload
    Whatspp Spam Code

---
+++ Whatsapp spam-code.py
@@ -0,0 +1,12 @@
+import pyautogui as pt
+import time
+
+limit=input("Enter the Limit")
+msg=input("Enter Your Message")
+c=1
+
+time.sleep(4)
+for i in range(int(limit)):
+    pt.typewrite(msg+" "+str(c))
+    pt.press('Enter')
+    c+=1
